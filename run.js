9faa33f5e79317ef2382901ab0bb694fe61c515f
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2023-06-21 18:58:17 -0700

    fix: add debug information

--- run.js
+++ run.js
@@ -7,8 +7,14 @@ import { main } from "./main.js";
 import { $ } from "execa";
 
 const workspacePath = String(core.getInput("workspace-path"));
+const eventPath = String(core.getInput("event-path"));
+
+core.debug(`workspacePath: ${workspacePath}`);
+core.debug(`event_path: ${eventPath}`);
 
 /** @type {import("@octokit/webhooks-types").PullRequestEvent} */
-const event = JSON.parse(readFileSync(core.getInput("event-path"), "utf8"));
+const event = JSON.parse(readFileSync(eventPath, "utf8"));
+
+core.debug(`event: ${JSON.stringify(event, null, 2)}`);
 
 main(workspacePath, event, core, $);
